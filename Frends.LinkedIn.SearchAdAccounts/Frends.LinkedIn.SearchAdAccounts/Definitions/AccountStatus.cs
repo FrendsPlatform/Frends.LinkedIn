32bfd08a9b9fc1c5eede1ab2dea92a05882bb11f
Riku Virtanen <virtanen.riku91@gmail.com>
2023-10-05 13:28:47 +0300

    Initial implementation

---
+++ Frends.LinkedIn.SearchAdAccounts/Frends.LinkedIn.SearchAdAccounts/Definitions/AccountStatus.cs
@@ -0,0 +1,15 @@
+﻿namespace Frends.LinkedIn.SearchAdAccounts.Definitions;
+
+/// <summary>
+/// Campaign status filters.
+/// </summary>
+public enum AccountStatus
+{
+#pragma warning disable CS1591 // self-explanatory
+    ACTIVE,
+    DRAFT,
+    CANCELED,
+    PENDING_DELETION,
+    REMOVED,
+#pragma warning restore CS1591 // self-explanatory
+}
---
+++ Frends.LinkedIn.SearchAdAccounts/Frends.LinkedIn.SearchAdAccounts/Definitions/TypeFilter.cs
@@ -0,0 +1,13 @@
+﻿namespace Frends.LinkedIn.SearchAdAccounts.Definitions;
+
+/// <summary>
+/// Filter object used with LinkedIn request.
+/// </summary>
+public class TypeFilter
+{
+    /// <summary>
+    /// Campaign type filter.
+    /// </summary>
+    /// <example>AccountType.BUSINESS</example>
+    public AccountType AccountType { get; set; }
+}
