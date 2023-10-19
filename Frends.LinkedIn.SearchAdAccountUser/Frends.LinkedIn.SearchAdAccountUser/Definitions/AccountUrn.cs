0c35e987a18e699f54e5fc701effb992e1ad2720
Riku Virtanen <virtanen.riku91@gmail.com>
2023-10-19 10:30:10 +0300

    Initial implementation

---
+++ Frends.LinkedIn.SearchAdAccountUser/Frends.LinkedIn.SearchAdAccountUser/Definitions/AccountUrn.cs
@@ -0,0 +1,12 @@
+﻿namespace Frends.LinkedIn.SearchAdAccountUser.Definitions;
+
+/// <summary>
+/// Class for Accoutn Urn.
+/// </summary>
+public class AccountUrn
+{
+    /// <summary>
+    /// Urn for the account.
+    /// </summary>
+    public string Urn { get; set; }
+}
---
+++ Frends.LinkedIn.SearchAdAccountUser/Frends.LinkedIn.SearchAdAccountUser/Definitions/Filter.cs
@@ -0,0 +1,42 @@
+﻿namespace Frends.LinkedIn.SearchAdAccountUser.Definitions;
+
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+/// <summary>
+/// Input class.
+/// </summary>
+public class Filter
+{
+    /// <summary>
+    /// Enumeration to choose how the search is done.
+    /// </summary>
+    /// <example>SearchAttribute.FindAdAccountsByAuthenticatedUser</example>
+    public SearchAttribute SearchMethod { get; set; }
+
+    /// <summary>
+    /// Searches for users with AdAccount URN.
+    /// </summary>
+    /// <example>urn:li:sponsoredAccount:516986977</example>
+    [UIHint(nameof(SearchMethod), "", SearchAttribute.GetAdAccountUser)]
+    [DefaultValue("")]
+    [DisplayFormat(DataFormatString = "Text")]
+    public string AccountUrn { get; set; }
+
+    /// <summary>
+    /// Searches for users with AdAccount URN.
+    /// </summary>
+    /// <example>[ urn:li:sponsoredAccount:516986977 ]</example>
+    [UIHint(nameof(SearchMethod), "", SearchAttribute.FindAdAccountUsersByAccounts)]
+    [DefaultValue("")]
+    public AccountUrn[] AccountUrns { get; set; }
+
+    /// <summary>
+    /// Searches for users with person URN.
+    /// </summary>
+    /// <example>urn:li:person:_mVMF2Kp8p</example>
+    [UIHint(nameof(SearchMethod), "", SearchAttribute.GetAdAccountUser)]
+    [DefaultValue("")]
+    [DisplayFormat(DataFormatString = "Text")]
+    public string UserUrn { get; set; }
+}
