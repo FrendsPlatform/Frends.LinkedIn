5b00040006f5683e31261e288821a4b9918f8206
Mateusz Noga-Wojtania <mateusz.noga-wojtania@frends.com>
2025-03-18 12:20:10 +0100

    update packages for SearchAdAnalytics task

--- Frends.LinkedIn.SearchAdAnalytics/Frends.LinkedIn.SearchAdAnalytics.Tests/UnitTests.cs
+++ Frends.LinkedIn.SearchAdAnalytics/Frends.LinkedIn.SearchAdAnalytics.Tests/UnitTests.cs
@@ -5,6 +5,7 @@ namespace Frends.LinkedIn.SearchAdAnalytics.Tests
     using System.Threading.Tasks;
     using Frends.LinkedIn.SearchAdAnalytics.Definitions;
     using NUnit.Framework;
+    using NUnit.Framework.Legacy;
 
     [TestFixture]
     internal class UnitTests
@@ -45,7 +46,7 @@ namespace Frends.LinkedIn.SearchAdAnalytics.Tests
         public async Task SearchAdAnalytics_BasicTest()
         {
             var result = await LinkedIn.SearchAdAnalytics(_filter, _options, default);
-            Assert.AreEqual(200, result.StatusCode);
+            ClassicAssert.AreEqual(200, result.StatusCode);
         }
 
         [Test]
@@ -53,7 +54,7 @@ namespace Frends.LinkedIn.SearchAdAnalytics.Tests
         {
             _filter.Metrics = new string[] { "clicks", "comments" };
             var result = await LinkedIn.SearchAdAnalytics(_filter, _options, default);
-            Assert.AreEqual(200, result.StatusCode);
+            ClassicAssert.AreEqual(200, result.StatusCode);
         }
 
         [Test]
@@ -85,12 +86,12 @@ namespace Frends.LinkedIn.SearchAdAnalytics.Tests
                 "opens",
             };
             var ex = Assert.ThrowsAsync<WebException>(async () => await LinkedIn.SearchAdAnalytics(_filter, _options, default));
-            Assert.IsTrue(ex.Message.Contains(@"""status"": 400"));
-            Assert.IsTrue(ex.Message.Contains(@"""message"": ""Too many fields requested. Maximum possible fields to request: 20"""));
+            ClassicAssert.IsTrue(ex.Message.Contains(@"""status"": 400"));
+            ClassicAssert.IsTrue(ex.Message.Contains(@"""message"": ""Too many fields requested. Maximum possible fields to request: 20"""));
 
             _options.ThrowExceptionOnErrorResponse = false;
             var result = await LinkedIn.SearchAdAnalytics(_filter, _options, default);
-            Assert.AreEqual(400, result.StatusCode);
+            ClassicAssert.AreEqual(400, result.StatusCode);
         }
 
         [Test]
@@ -98,11 +99,11 @@ namespace Frends.LinkedIn.SearchAdAnalytics.Tests
         {
             _options.Token = "invalid_access_token"; // Simulate an invalid token
             var ex = Assert.ThrowsAsync<WebException>(async () => await LinkedIn.SearchAdAnalytics(_filter, _options, default));
-            Assert.IsTrue(ex.Message.Contains(@"""status"": 401"));
+            ClassicAssert.IsTrue(ex.Message.Contains(@"""status"": 401"));
 
             _options.ThrowExceptionOnErrorResponse = false;
             var result = await LinkedIn.SearchAdAnalytics(_filter, _options, default);
-            Assert.AreEqual(401, result.StatusCode);
+            ClassicAssert.AreEqual(401, result.StatusCode);
         }
     }
 }
